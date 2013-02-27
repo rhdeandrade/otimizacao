a6f61ce9565d5e07793393872b8964e2272902e5
Raphael de Andrade <rhdeandrade@gmail.com>
2013-02-26 23:51:17 -0300

    Added missing file

---
+++ despachohidrotermico/hill_climbing.cpp
@@ -0,0 +1,45 @@
+#ifndef hill_climbing_h
+#define hill_climbing_h
+
+#include <iostream>
+
+using namespace std;
+
+class HillClimbing {
+  public:
+    PlanoProducao current_state;
+    PlanoProducao next_state;
+
+    int maximum_iterations_number;
+    int maximum_perturbation_number_iteration;
+
+    HillClimbing(PlanoProducao p, int m, int mp);
+    PlanoProducao execute(int operacao_atomica);
+    void perturbation(int operacao_atomica, int counter);
+};
+
+HillClimbing::HillClimbing(PlanoProducao p, int m, int mp) {
+  this->current_state = p;
+  this->maximum_iterations_number = m;
+  this->maximum_perturbation_number_iteration = mp;
+}
+
+PlanoProducao HillClimbing::execute(int operacao_atomica) {
+  int counter = 1;
+  while (1) {
+    this->perturbation(operacao_atomica, counter);
+  }
+  
+}
+
+void HillClimbing::perturbation(int operacao_atomica, int counter) {
+  PlanoProducao p(&this->current_state);
+
+  this->next_state = p;
+  for (int i; i < this->maximum_perturbation_number_iteration; i++) {
+    this->next_state.perturbation(operacao_atomica, counter);
+
+  }
+}
+
+#endif
