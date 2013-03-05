6c307d763a291b7b85d00ef5a05a1628fd1452d3
Raphael de Andrade <rhdeandrade@gmail.com>
2013-03-05 20:36:04 -0300

    Print results

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "despachohidrotermico/otimizacao_despacho_hidrotermico.cpp"
 #include "despachohidrotermico/plano_producao.h"
 #include "despachohidrotermico/otimizacao_despacho_hidrotermico_globals.h"
+#include "util/report.cpp"
 #include <ctime>
 
 using namespace std;
@@ -21,6 +22,8 @@ int main() {
   int operacao_atomica = 4;
   cout << "Executando Hill Climbing\n";
   odh.executar_hill_climbing(operacao_atomica, 60, 3);
+
+  Report::imprimir_resultados(odh.plano_producao);
 
   return 0;
 }
--- util/report.cpp
+++ util/report.cpp
@@ -16,14 +16,25 @@ void Report::imprimir_resultados(PlanoProducao plano_producao) {
     total_energia_hidraulica += plano_producao.hidreletricas.at(i).total_energia_gerada();
   }
 
-  cout << "Total Energia Hidraulica: " << total_energia_gerada << "\n";
+  cout << "Total Energia Hidraulica: " << total_energia_hidraulica << "\n";
 
   double total_energia_termica = 0;
   for (int i = 0; i < plano_producao.termicas.size(); i++) {
-    total_energia_termica += plano.termicas.at(i).total_energia_gerada();
+    total_energia_termica += plano_producao.termicas.at(i).total_energia_gerada();
   }
 
-  
+  cout << "Total Energia Termica: " << total_energia_termica << "\n";
+
+  double total_deficit = 0;
+
+  for (int i = 0; i < plano_producao.subsistemas.size(); i++) {
+    for (int j = 0; j < plano_producao.subsistemas.at(i).deficits.size(); j++) {
+      total_deficit += plano_producao.subsistemas.at(i).deficits.at(j).deficit;
+    }
+  }
+
+  cout << "Total Deficit: " << total_deficit << "\n";
+
 }
 
 
